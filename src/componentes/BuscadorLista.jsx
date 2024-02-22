4bb6bd946ec8fca588ac340b2e71ebd2788ab423
Deoz <danielvalenal99@gmail.com>
2024-02-22 11:23:54 -0700

    fix search list

--- src/componentes/BuscadorLista.jsx
+++ src/componentes/BuscadorLista.jsx
@@ -14,7 +14,7 @@ function BuscadorLista({ data, fotos }) {
         <Link key={index} to={`/especie/${element.id}`}>
           <ListGroup.Item >
             <div className='d-flex'>
-              <Image width={75} height={75} src={element.default_photo.square_url == null ? "src/assets/img/image_not_available.png" : element.default_photo.square_url} roundedCircle />
+              <Image width={75} height={75} src={element.default_photo == null ? "src/assets/img/image_not_available.png" : element.default_photo.square_url} roundedCircle />
               <p className='mt-auto mb-auto ms-4'> {element.preferred_common_name} ( <i className='cursiva'>{element.name} </i> )</p>
             </div>
           </ListGroup.Item>
